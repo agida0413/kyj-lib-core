de5c3d022da4a12f287685ca401a83fa6a43e262
kimyj <yongjjun8718@gmail.com>
2025-10-04 00:46:42 +0900

    AuthMemberDTo Builder

--- kyj-lib-core-security-auth/src/main/java/com/kyj/core/security/auth/dto/AuthMemberDTO.java
+++ kyj-lib-core-security-auth/src/main/java/com/kyj/core/security/auth/dto/AuthMemberDTO.java
@@ -1,10 +1,12 @@
 package com.kyj.core.security.auth.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class AuthMemberDTO {
     private Long userId;
     private String username;
